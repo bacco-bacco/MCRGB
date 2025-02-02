a0d07954ae84c6214cdce95119ffcb6b1b35573c
Bacco <jacobherd1@yahoo.ie>
2025-02-02 15:22:54 +0000

    0.3 1.21.4

--- src/client/java/com/bacco/gui/WColourGuiSlot.java
+++ src/client/java/com/bacco/gui/WColourGuiSlot.java
@@ -99,7 +99,7 @@ public class WColourGuiSlot extends WWidget{
    @Environment(EnvType.CLIENT)
    @Override
    public void addTooltip(TooltipBuilder tooltip) {
-      tooltip.add(Text.translatable(stack.getTranslationKey()));
+      tooltip.add(stack.getItemName());
       IItemBlockColourSaver item = (IItemBlockColourSaver) stack.getItem();
 			for(int i = 0; i < item.getLength(); i++){
 				ArrayList<String> strings = item.getSpriteDetails(i).getStrings();
