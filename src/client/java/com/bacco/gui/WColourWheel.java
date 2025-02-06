77118b483acf1e83ec5a9f8104abdadd181a3f6c
Bacco <jacobherd1@yahoo.ie>
2025-02-06 21:20:09 +0000

    colour wheel transparent pixels bugfix

--- src/client/java/com/bacco/gui/WColourWheel.java
+++ src/client/java/com/bacco/gui/WColourWheel.java
@@ -14,6 +14,9 @@ public class WColourWheel extends WPickableTexture{
     int cursorY = (height)/2;
     Boolean beenClicked = false;
 
+
+
+
     public WColourWheel(Identifier image, float u1, float v1, float u2, float v2, MinecraftClient client, MCRGBBaseGui gui) {
         super(image, u1, v1, u2, v2, client, gui);
     }
@@ -35,21 +38,31 @@ public class WColourWheel extends WPickableTexture{
     @Override
     public InputResult onClick(int containerX, int containerY, int button) {
         beenClicked = true;
-        cursorX = containerX;
-        cursorY = containerY;
-        return super.onClick(containerX,containerY,button);
+        InputResult ret = super.onClick(containerX,containerY,button);
+        if(isTransparent) return ret;
+        if(!(containerX < 0 || containerY < 0 || containerX >= width || containerY >= height)) {
+            cursorX = containerX;
+            cursorY = containerY;
+        }
+        return ret;
     }
     @Environment(EnvType.CLIENT)
     @Override
     public InputResult onMouseDrag(int containerX, int containerY, int mouseButton, double deltaX, double deltaY) {
         beenClicked = true;
-        cursorX = containerX;
-        cursorY = containerY;
-        return super.onMouseDrag(containerX, containerY, mouseButton, deltaX, deltaY);
+        InputResult ret = super.onMouseDrag(containerX, containerY, mouseButton, deltaX, deltaY);
+        System.out.println(isTransparent);
+        if(isTransparent) return ret;
+        if(!(containerX < 0 || containerY < 0 || containerX >= width || containerY >= height)) {
+            cursorX = containerX;
+            cursorY = containerY;
+        }
+
+        return ret;
     }
 
     public void pickAtCursor(){
-        pickColour(cursorX,cursorY);
+        isTransparent = pickColour(cursorX,cursorY);
     }
 
     /*@Override
