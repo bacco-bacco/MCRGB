9730f80bea3a71d608b1ed4193db7bc80028cc16
Bacco <jacobherd1@yahoo.ie>
2026-01-01 17:48:16 +0000

    ported 0.3.2 features from 1.20.1 branch

--- src/client/java/com/bacco/MCRGBClient.java
+++ src/client/java/com/bacco/MCRGBClient.java
@@ -259,20 +259,16 @@ public class MCRGBClient implements ClientModInitializer {
 			totalBlocks +=1;
 			Set<Sprite> sprites = new HashSet<Sprite>();
 			//try to get the default top texture sprite. if fails, report error and skip this block
-			
+			Direction[] directions = {Direction.UP,Direction.DOWN,Direction.NORTH,Direction.SOUTH,Direction.EAST,Direction.WEST,null};
 			block.getStateManager().getStates().forEach(state -> {
-				try{
-					var model = client.getBakedModelManager().getBlockModels().getModel(state);						
-					sprites.add(model.getQuads(state, Direction.UP, Random.create()).get(0).getSprite());
-					sprites.add(model.getQuads(state, Direction.DOWN, Random.create()).get(0).getSprite());
-					sprites.add(model.getQuads(state, Direction.NORTH, Random.create()).get(0).getSprite());
-					sprites.add(model.getQuads(state, Direction.SOUTH, Random.create()).get(0).getSprite());
-					sprites.add(model.getQuads(state, Direction.EAST, Random.create()).get(0).getSprite());
-					sprites.add(model.getQuads(state, Direction.WEST, Random.create()).get(0).getSprite());
-					successes +=1;
-				}catch(Exception e){	
-					fails +=1;						
-					return;
+				for(int i = 0; i < directions.length; i++){
+					try{
+						var model = client.getBakedModelManager().getBlockModels().getModel(state);
+						sprites.add(model.getQuads(state, directions[i], Random.create()).get(0).getSprite());
+						successes +=1;
+					}catch(Exception e){
+						fails +=1;
+					}
 				}
 			});
 			if(sprites.size() < 1){
