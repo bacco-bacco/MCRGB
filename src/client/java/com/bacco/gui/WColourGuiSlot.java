093057e812a79b15819c28903921ff36c57760b8
Bacco <jacobherd1@yahoo.ie>
2026-03-19 20:49:00 +0000

    Correctly drawing stack numbers and dura bar

--- src/client/java/com/bacco/gui/WColourGuiSlot.java
+++ src/client/java/com/bacco/gui/WColourGuiSlot.java
@@ -51,7 +51,11 @@ public class WColourGuiSlot extends WWidget{
    @Override
    public void paint(DrawContext context, int x, int y, int mouseX, int mouseY) {
       ScreenDrawing.texturedRect(context, x, y, 18, 18, SLOT_TEXTURE, 0, 0, .28125f, .28125f, 0xFFFFFFFF);
-      if(stack!=null)   context.drawItem(stack, x+1, y+1);
+      if(stack!=null)   {
+         context.drawItem(stack, x+1, y+1);
+         context.drawItemInSlot(MinecraftClient.getInstance().textRenderer, stack, x+1, y+1);
+      }
+
 
    }
 
